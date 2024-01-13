7291d58fa772bec6c0e676224b03d7d93162109b
michaelwhendrix <michael.w.hendrix@gmail.com>
2024-01-12 21:54:22 -0600

    displays all products and has working filter bar for categories

--- src/client/components/AllProducts.jsx
+++ src/client/components/AllProducts.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from "react";
 import axios from "axios";
+import FilterBar from "./FilterBar";
 
 const AllProducts = () => {
 
@@ -21,9 +22,27 @@ useEffect(() => {
 
 
     return (
-        <>
+        <div className="all-products">
             <h1>All Products</h1>
-        </>
+            <div className="main">
+            <FilterBar products={products}/>
+            <div className="cards">
+                {products && products.map((product)=> {
+                    return (
+                        <div key={product.id} style={{height:"300px", width:"300px", border:"black solid 4px", margin:"20px", backgroundColor:"gold", color:"black"}}>
+                            <h3>{product.name}</h3>
+                            <p>{product.description}</p>
+                            <img src={product.imageUrl} style={{height:"100px", width:"100px"}}/>
+
+                        </div>
+                    )
+
+                }
+                    
+                )}
+            </div>
+            </div>
+        </div>
     );
 };
 
--- src/client/components/FilterBar.jsx
+++ src/client/components/FilterBar.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FilterBar = () => {
+
+
+const FilterBar = ({products}) => {
+    const [category, setCategory] = useState('1');
+    const [bidTime, setBidTime] = useState('');
+
+    const filterCategories = async(e) => {
+        e.preventDefault();
+        console.log(category);
+        console.log(products, category);
+        const filteredCategoryResults = await products.filter(
+            (product) => {
+                return product.categoryId === +category;
+            }
+        );
+        console.log(filteredCategoryResults);
+    }
+
     return (
         <>
-        
+            <form className="filter-bar" onSubmit={filterCategories}>
+                <label>Category
+                        <select name="selectedCategory" defaultValue={category} onChange={(e)=>setCategory(e.target.value)}>
+                            <option value="1">Auto</option>
+                            <option value="2">Books</option>
+                            <option value="3">Clothes</option>
+                            <option value="4">Collectables</option>
+                            <option value="5">Electronics</option>
+                            <option value="6">Furniture</option>
+                            <option value="7">Games</option>
+                            <option value="8">Jewelry</option>
+                            <option value="9">Kitchen</option>
+                            <option value="10">Sports</option>
+                            <option value="11">Toys</option>
+                        </select>
+                </label>
+                <label>Bid Time
+                        <select name="selectedTime" defaultValue={bidTime}>
+                            <option value="newest">Newest</option>
+                            <option value="expiring">Soonest Expiring</option>
+                        </select>
+                </label>
+                <button type="reset">Reset</button>
+                <button type="submit">Submit</button>
+            </form>
         </>
     );
 };
