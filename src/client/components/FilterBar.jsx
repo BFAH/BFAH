8b28b8469d361aa1e59022d75bdeba21bc44c068
michaelwhendrix <michael.w.hendrix@gmail.com>
2024-01-16 09:42:10 -0600

    added priceRange and cleaned up filter function

--- src/client/components/FilterBar.jsx
+++ src/client/components/FilterBar.jsx
@@ -3,8 +3,9 @@ import React, { useState } from "react";
 
 
 const FilterBar = ({products,setFiltered}) => {
-    const [category, setCategory] = useState('1');
+    const [category, setCategory] = useState([1,2,3,4,5,6,7,8,9,10,11]);
     const [bidTime, setBidTime] = useState('');
+    const [priceRange, setPriceRange] = useState('99999999999');
 
     const filterCategories = async(e) => {
         e.preventDefault();
@@ -12,10 +13,13 @@ const FilterBar = ({products,setFiltered}) => {
         console.log(products, category);
         const filteredCategoryResults = await products.filter(
             (product) => {
-                return product.categoryId === +category;
+                return category.includes(product.categoryId) &&
+                product.price < +priceRange;
             }
         );
         setFiltered(filteredCategoryResults);
+        setCategory([1,2,3,4,5,6,7,8,9,10,11]);
+        setPriceRange('999999999999');
     }
 
     return (
@@ -23,6 +27,7 @@ const FilterBar = ({products,setFiltered}) => {
             <form className="filter-bar" onSubmit={filterCategories}>
                 <label>Category
                         <select name="selectedCategory" defaultValue={category} onChange={(e)=>setCategory(e.target.value)}>
+                            <option value="[1,2,3,4,5,6,7,8,9,10,11]">All</option>
                             <option value="1">Auto</option>
                             <option value="2">Books</option>
                             <option value="3">Clothes</option>
@@ -41,6 +46,14 @@ const FilterBar = ({products,setFiltered}) => {
                             <option value="newest">Newest</option>
                             <option value="expiring">Soonest Expiring</option>
                         </select>
+                </label>
+                <label>Price Range
+                    <select name="selectedPrice" defaultValue={priceRange} onChange={(e) => {setPriceRange(e.target.value)}}>
+                        <option value="999999999">All</option>
+                        <option value="300">less 300</option>
+                        <option value="600"> less 600</option>
+                        <option value="2000">less 2000</option>
+                    </select>
                 </label>
                 <button type="reset" onClick={()=> {setFiltered(null)}}>Reset</button>
                 <button type="submit">Submit</button>
