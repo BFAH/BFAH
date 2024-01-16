c21fb1169c122a9aa006e44f8fcb735bcfc1e708
r33twaun <r33twaun@gmail.com>
2024-01-16 12:20:06 -0500

    made register endpoint

--- src/server/auth/index.js
+++ src/server/auth/index.js
@@ -1,11 +1,33 @@
-const {PrismaClient} = require('@prisma/client')
-const prisma = new (PrismaClient)
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const router = require("express").Router();
 
 //PORS /auth/register creates a user with a hased password
-router.post('/register', ) 
+router.post("/register", async (req, res, next) => {
+  const { email, username, password } = req.body;
+  const SALT_ROUNDS = 5;
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        isAdmin: false,
+        username,
+      },
+    });
+    const token = jwt.sign(
+      { id: user.id, email: user.email, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET
+    );
+    res.status(201).send({ token });
+  } catch (err) {
+    console.error("ERROR - Could Not REGISTER!!!", err);
+    res.status(500).json({ err: "SERVER ERROR" });
+  }
+});
 
 module.exports = router;
