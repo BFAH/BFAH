71fbddf3fca4b12a42b4dc32c20dd2604b311003
aa-shin <aa.shin@yahoo.com>
2024-01-17 14:45:26 -0500

    add navbar in navigation.jsx

--- src/client/components/Navigation.jsx
+++ src/client/components/Navigation.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-    return (
-        <>
-        
-        </>
-    );
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/account">My Account</Link>
+      <Link to="/sell">Sell</Link>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <button type="submit">Go!</button>
+      </form>
+    </nav>
+  );
 };
 
 export default Navigation;
