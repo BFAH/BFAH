e38dfe655e9fcc65116d56377a4b161a736b7b7b
r33twaun <r33twaun@gmail.com>
2024-01-18 16:18:18 -0500

    changed files to pretty format

--- src/client/components/AllProducts.jsx
+++ src/client/components/AllProducts.jsx
@@ -1,87 +1,114 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import FilterBar from "./FilterBar";
 import { Link } from "react-router-dom";
 import Auction from "./Auction";
 
 const AllProducts = () => {
+  const [products, setProducts] = useState();
+  const [filtered, setFiltered] = useState(null);
+  const [auctionData, setAuctionData] = useState();
 
-    const [products, setProducts] = useState();
-    const [filtered, setFiltered] = useState(null);
-    const [auctionData, setAuctionData] = useState();
-
-
-
-useEffect(() => {
+  useEffect(() => {
     const fetchAllProducts = async () => {
-        try {
-            const response = await axios.get('/api/products');
-            setProducts(response.data);
-            const response2 = await axios.get(`/api/auctions`);
-            setAuctionData(response2.data);
-        } catch (error) {
-            console.error('ERROR - Could Not Fetch All Products', error);
-        }
+      try {
+        const response = await axios.get("/api/products");
+        setProducts(response.data);
+        const response2 = await axios.get(`/api/auctions`);
+        setAuctionData(response2.data);
+      } catch (error) {
+        console.error("ERROR - Could Not Fetch All Products", error);
+      }
     };
-fetchAllProducts();
-
-}, []);
-if(auctionData){
-    
-    for(let auction of auctionData) {
-        products[auction.productId].bidTime = new Date(auction.bidEndTime).toLocaleString();
-        products[auction.productId].currentBid = auction.currentBidPrice;
+    fetchAllProducts();
+  }, []);
+  if (auctionData) {
+    for (let auction of auctionData) {
+      products[auction.productId].bidTime = new Date(
+        auction.bidEndTime
+      ).toLocaleString();
+      products[auction.productId].currentBid = auction.currentBidPrice;
     }
-}
-    return (
-        <div className="all-products">
-            <h1>All Products</h1>
-            <div className="main">
-                <FilterBar products={products} setFiltered={setFiltered}/>
-                {filtered ? 
-                <div className="cards">
-                    {filtered && filtered.map((product)=> {
-                        return (
-                            
-                            <div key={product.id} >
-                                <Link to={`/${product.id}`} style={{textDecoration:"none"}}>
-                                <div style={{height:"380px", width:"300px", border:"black solid 4px", margin:"20px", backgroundColor:"#def2f1", color:"black"}}>
-                                <h3>{product.name}</h3>
-                                <p>{product.description}</p>
-                                <img src={product.imageUrl} style={{height:"100px", width:"100px"}}/>
-                                <h4>Buy Now: ${product.price}</h4>
-                                <h4>Current Bid: ${product.currentBid}</h4>
-                                <h5>Bid End: {product.bidTime}</h5>
-                                </div>
-                                </Link>
-                            </div>
-                                 )})}
-                </div>
-                            :
-
-                <div className="cards">
-                    {products && products.map((product)=> {
-                        return (
-                           
-                            <div key={product.id} >
-                                <Link to={`/${product.id}`} style={{textDecoration:"none"}}>
-                                <div style={{height:"380px", width:"300px", border:"black solid 4px", margin:"20px", backgroundColor:"#def2f1", color:"black"}}>
-                                <h3>{product.name}</h3>
-                                <p>{product.description}</p>
-                                <img src={product.imageUrl} style={{height:"100px", width:"100px"}}/>
-                                <h4>Buy Now: ${product.price}</h4>
-                                <h4>Current Bid: ${product.currentBid}</h4>
-                                <h5>Bid End: {product.bidTime}</h5>
-                                </div>
-                                </Link>
-                                
-                            </div>
-                            
-                        )})}
-                </div> }
-            </div>
-        </div>
-    );
+  }
+  return (
+    <div className="all-products">
+      <h1>All Products</h1>
+      <div className="main">
+        <FilterBar products={products} setFiltered={setFiltered} />
+        {filtered ? (
+          <div className="cards">
+            {filtered &&
+              filtered.map((product) => {
+                return (
+                  <div key={product.id}>
+                    <Link
+                      to={`/${product.id}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <div
+                        style={{
+                          height: "380px",
+                          width: "300px",
+                          border: "black solid 4px",
+                          margin: "20px",
+                          backgroundColor: "#def2f1",
+                          color: "black",
+                        }}
+                      >
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                        <img
+                          src={product.imageUrl}
+                          style={{ height: "100px", width: "100px" }}
+                        />
+                        <h4>Buy Now: ${product.price}</h4>
+                        <h4>Current Bid: ${product.currentBid}</h4>
+                        <h5>Bid End: {product.bidTime}</h5>
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+        ) : (
+          <div className="cards">
+            {products &&
+              products.map((product) => {
+                return (
+                  <div key={product.id}>
+                    <Link
+                      to={`/${product.id}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <div
+                        style={{
+                          height: "380px",
+                          width: "300px",
+                          border: "black solid 4px",
+                          margin: "20px",
+                          backgroundColor: "#def2f1",
+                          color: "black",
+                        }}
+                      >
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                        <img
+                          src={product.imageUrl}
+                          style={{ height: "100px", width: "100px" }}
+                        />
+                        <h4>Buy Now: ${product.price}</h4>
+                        <h4>Current Bid: ${product.currentBid}</h4>
+                        <h5>Bid End: {product.bidTime}</h5>
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AllProducts;
--- src/server/api/users.js
+++ src/server/api/users.js
@@ -1,54 +1,62 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const router = require("express").Router();
-const {verify} = require('../util')
+const { verify } = require("../util");
 
 //GET returns all users
-router.get('/', async (req, res, next) => {
-	try {
-		const users = await prisma.user.findMany();
-		res.status(200).send(users)
-	} catch (err) {
-		console.error(err);
-	}
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).send(users);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 //GET returns user by id
-router.get('/:id', async (req, res, next) => {
-	const { id } = req.params
-	try {
-		const user = await prisma.user.findUnique({
-			where: {
-				id: +id
-			}
-		})
-		res.status(200).send(user);
-	} catch (err) {
-		console.error();
-	}
-})
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: +id,
+      },
+    });
+    res.status(200).send(user);
+  } catch (err) {
+    console.error();
+  }
+});
 
 //GET returns logged in user's ID
-router.get('/current/account', verify, async (req, res, next) => {
-	try {
-		const currentUser = await prisma.user.findFirst({
-			where: {
-				id: req.user.id,
-			}
-		});
-		res.status(200).send(currentUser);
-	} catch (err) {
-		console.error(err);
-	}
-})
+router.get("/current/account", verify, async (req, res, next) => {
+  try {
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: req.user.id,
+      },
+    });
+    res.status(200).send(currentUser);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 //POST creates users account profile information
-router.post('/account/create', verify, async (req, res, next) => {
-    const {firstName, lastName, streetAddress, city, state, 
-        zipCode, country, phoneNumber} = req.body;
-        try {
-const accountInfo = await prisma.account.create({
-    data: {
+router.post("/account/create", verify, async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    streetAddress,
+    city,
+    state,
+    zipCode,
+    country,
+    phoneNumber,
+  } = req.body;
+  try {
+    const accountInfo = await prisma.account.create({
+      data: {
         firstName,
         lastName,
         streetAddress,
@@ -57,25 +65,34 @@ const accountInfo = await prisma.account.create({
         zipCode,
         country,
         phoneNumber,
-        userId: req.user.id
-    }
-})
-res.status(201).send(accountInfo)
-        } catch(err) {
-            console.error(err)
-        }
-})
+        userId: req.user.id,
+      },
+    });
+    res.status(201).send(accountInfo);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 //PATCH users can edit their account profile information
-router.patch('/account/edit', verify, async (req, res, next) => {
-    const {id, firstName, lastName, streetAddress, city, state, 
-        zipCode, country, phoneNumber} = req.body;
-        try {
-const account = await prisma.account.update({
-    where: {
-        id: +id
-    },
-    data: {
+router.patch("/account/edit", verify, async (req, res, next) => {
+  const {
+    id,
+    firstName,
+    lastName,
+    streetAddress,
+    city,
+    state,
+    zipCode,
+    country,
+    phoneNumber,
+  } = req.body;
+  try {
+    const account = await prisma.account.update({
+      where: {
+        id: +id,
+      },
+      data: {
         firstName,
         lastName,
         streetAddress,
@@ -84,48 +101,48 @@ const account = await prisma.account.update({
         zipCode,
         country,
         phoneNumber,
-    }
-})
-res.status(201).send(account)
-        } catch(err) {
-            console.error(err)
-        }
-})
+      },
+    });
+    res.status(201).send(account);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 //PATCH updates user isAdmin to true
-router.patch('/admin', async (req, res, next) => {
-	const { id } = req.body;
-	try {
-		const updateUser = await prisma.user.update({
-			where: {
-				id: +id
-			},
-			data: {
-				isAdmin: true
-			}
-		})
-		res.status(201).send(updateUser)
-	} catch (err) {
-		console.error(err)
-	}
-})
+router.patch("/admin", async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    const updateUser = await prisma.user.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        isAdmin: true,
+      },
+    });
+    res.status(201).send(updateUser);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 //PATCH updates user isAdmin to false
-router.patch('/admin/remove', async (req, res, next) => {
-	const { id } = req.body;
-	try {
-		const updateUser = await prisma.user.update({
-			where: {
-				id: +id
-			},
-			data: {
-				isAdmin: false
-			}
-		})
-		res.status(201).send(updateUser)
-	} catch (err) {
-		console.error(err)
-	}
-})
+router.patch("/admin/remove", async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    const updateUser = await prisma.user.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        isAdmin: false,
+      },
+    });
+    res.status(201).send(updateUser);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 module.exports = router;
