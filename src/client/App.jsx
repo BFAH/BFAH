d67952997525907e1796a20dd2109934e09f0c95
r33twaun <r33twaun@gmail.com>
2024-01-29 14:53:54 -0500

    reformatted navBar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,10 +6,6 @@ import Navigation from "./components/Navigation";
 function App() {
   return (
     <>
-      <img
-        src="./Logo_art.jpg"
-        style={{ width: "900px", border: "black double 10px" }}
-      />
       <Navigation />
     </>
   );
