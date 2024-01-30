6ba2a681eb22c8d8221786e91a9f43e5d6797476
aa-shin <aa.shin@yahoo.com>
2024-01-30 15:07:19 -0500

    add confirmation message

--- src/client/components/Confirmation.jsx
+++ src/client/components/Confirmation.jsx
@@ -1,5 +1,35 @@
-function Confirmation() {
-    return <h1>ORDER CONFIRMED!</h1>
-}
+import React from 'react';
 
-export default Confirmation
+const Confirmation = ({ auctionData }) => {
+  return (
+		<>
+			<h1>Order Confirmed. Payment Processed. Thank You!</h1>
+			{/* <div className="cards">
+				<div>
+					<div
+						style={{
+							height: '380px',
+							width: '300px',
+							border: 'black solid 4px',
+							margin: '20px',
+							backgroundColor: '#def2f1',
+							color: 'black',
+						}}
+					>
+						<h3>{auctionData.product.id}</h3>
+						<p>{auctionData.product.description}</p>
+						<img
+							src={auctionData.product.imageUrl}
+							style={{ height: '100px', width: '100px' }}
+							alt="Product"
+						/>
+						<h4>Winning Bid: ${auctionData.currentBidPrice}</h4>
+						<h5>Bid End: {new Date(auctionData.bidEndTime).toLocaleString()}</h5>
+					</div>
+				</div>
+			</div> */}
+		</>
+  );
+};
+
+export default Confirmation;
