ddc24761aad3109befa8de19021592ff51d1819c
r33twaun <r33twaun@gmail.com>
2024-01-31 12:48:10 -0500

    users now have an accordion tab that displays auctions they have won

--- src/server/util.js
+++ src/server/util.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verify = (req, res, next) => {
     const bearer = req.headers.authorization;
+    console.log(bearer)
 
     if (!bearer) {
         res.status(401).send({message: "No token provided, NOT AUTHORIZED!"});
