08d3034f3407203588e4bd97c177db91ed205a1a
aa-shin <aa.shin@yahoo.com>
2024-02-01 12:30:55 -0500

    add auction acrd on confirmation page and style

--- src/client/components/Confirmation.jsx
+++ src/client/components/Confirmation.jsx
@@ -1,33 +1,78 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Card, ListGroup, Alert } from "react-bootstrap";
 
-const Confirmation = ({ auctionData }) => {
+const Confirmation = () => {
+	const [priceData, setPriceData] = useState('');
+  const [buyerData, setBuyerData] = useState('');
+	const [sellerId, setSellerId] = useState('');
+	const [sellerData, setSellerData] = useState('');
+	const [auctionData, setAuctionData] = useState({});
+
+	useEffect(()=> {
+    let auctionId = location.state.auctionId;
+    console.log(auctionId);
+  
+    const getData = async() => {
+      try {
+        const response2 = await axios.get(`/api/auctions/${+auctionId}`);
+        const response3 = await axios.get("/api/users/current/user", {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("TOKEN"),
+          },
+        });
+        setAuctionData(response2.data.products);
+        setPriceData(response2.data.currentBidPrice);
+        setProductData(response2.data.products.name);
+        setBuyerData(response3.data.username);
+        setStripePrice(response2.data.products.stripePriceId);
+        setSellerId(response2.data.userId);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  },[]);
+
+	useEffect(()=>{
+    const getSeller = async() => {
+      try {
+        const response = await axios.get(`/api/users/${+sellerId}`);
+        console.log(response.data);
+        setSellerData(response.data);
+        setSellerStripeAcct(response.data.stripeAccount);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getSeller();
+  },[sellerId]);
+
   return (
 		<>
-			<h1>Order Confirmed. Payment Processed. Thank You!</h1>
-			{/* <div className="cards">
-				<div>
-					<div
-						style={{
-							height: '380px',
-							width: '300px',
-							border: 'black solid 4px',
-							margin: '20px',
-							backgroundColor: '#def2f1',
-							color: 'black',
-						}}
-					>
-						<h3>{auctionData.product.id}</h3>
-						<p>{auctionData.product.description}</p>
-						<img
-							src={auctionData.product.imageUrl}
-							style={{ height: '100px', width: '100px' }}
-							alt="Product"
-						/>
-						<h4>Winning Bid: ${auctionData.currentBidPrice}</h4>
-						<h5>Bid End: {new Date(auctionData.bidEndTime).toLocaleString()}</h5>
-					</div>
-				</div>
-			</div> */}
+			<div className="cards">
+        <Card style={{ width: "80rem" }}>
+          <Alert variant="warning">
+						Order Confirmed. Payment Processed. Thank You!
+          </Alert>
+          <Card.Img
+            variant="top"
+            src={auctionData.imageUrl}
+            alt={auctionData.name}
+          />
+          <Card.Body>
+            <Card.Title>{auctionData.name}</Card.Title>
+            <Card.Text>{auctionData.description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Seller Username: 
+              <Card.Link href={`/store/${sellerId}`}> {sellerData.username}</Card.Link>
+            </ListGroup.Item>
+            <ListGroup.Item>Buyer Username: {buyerData}</ListGroup.Item>
+            <ListGroup.Item>Purchase Price: ${priceData}</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </div>
 		</>
   );
 };
